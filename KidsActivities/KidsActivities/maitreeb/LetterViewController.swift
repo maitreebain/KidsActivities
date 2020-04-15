bf38842f71d3327a36b0bae7e41fe8af39e09ff8
maitreebain <maitreebain@gmail.com>
2020-04-15 15:40:21 -0400

    letterview added to vc

--- KidsActivities/KidsActivities/maitreeb/LetterViewController.swift
+++ KidsActivities/KidsActivities/maitreeb/LetterViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class LetterViewController: UIViewController {
+    
+    private let letterView = LetterView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,15 +18,5 @@ class LetterViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
