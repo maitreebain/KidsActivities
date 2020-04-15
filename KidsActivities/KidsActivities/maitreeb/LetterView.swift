c6d1804a1a02c3b93dec8c456670ce55b1db1ee6
maitreebain <maitreebain@gmail.com>
2020-04-15 13:08:56 -0400

    added vc and view

---
+++ KidsActivities/KidsActivities/maitreeb/LetterView.swift
@@ -0,0 +1,21 @@
+//
+//  LetterView.swift
+//  KidsActivities
+//
+//  Created by Maitree Bain on 4/15/20.
+//  Copyright © 2020 Maitree Bain. All rights reserved.
+//
+
+import UIKit
+
+class LetterView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
